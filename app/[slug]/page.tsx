56da1236f99066820d6ffcc97d79bbc11a399b35
yoksel <juliahtml@gmail.com>
2023-12-16 23:01:45 +0000

    Fix headings

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,38 +1,26 @@
-import { useRouter } from 'next/router';
-import ErrorPage from 'next/error';
-// import Container from '../../components/container'
-// import PostBody from '../../components/post-body'
-// import Header from '../../components/header'
-// import PostHeader from '../../components/post-header'
-// import Layout from '../../components/layout'
 import { getPostBySlug } from '../utils/api';
-// import PostTitle from '../../components/post-title'
 import Head from 'next/head';
 import markdownToHtml from '../utils/markdownToHtml';
 import Layout from '../components/layout';
-// import { CMS_NAME } from '../../lib/constants'
-// import markdownToHtml from '../../lib/markdownToHtml'
-// import type PostType from '../../interfaces/post'
+import styles from './styles.module.scss';
 
 export default async function Post({ params }: any) {
   // const router = useRouter()
   // const { post, morePosts, preview } = props;
   // const title = `${post?.title} | Next.js Blog Example with ...`
-  const {slug} = params;
+  const { slug } = params;
   const post = getPostBySlug({
     slug,
-    fields: [
-      'title',
-      'slug',
-      'content',
-      'links',
-    ]});
+    fields: ['title', 'slug', 'content', 'links'],
+  });
   const content = await markdownToHtml(post.content || '');
 
   return (
     <Layout slug={params.slug}>
-      <h2>{post.title}</h2>
-      <div dangerouslySetInnerHTML={{ __html: content }}></div>
+      <article className={styles.post}>
+        <h1 className={styles.post__title}>{post.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: content }}></div>
+      </article>
     </Layout>
   );
 }
--- app/components/siteHeader/index.tsx
+++ app/components/siteHeader/index.tsx
@@ -1,12 +1,19 @@
 import SiteNav from '../siteNav';
 import styles from './siteHeader.module.scss';
 
-const SiteHeader = ({ slug }: { slug: string }) => (
-  <header className={styles.siteHeader}>
-    <h1>Простые правила разметки</h1>
+const SiteHeader = ({ slug }: { slug: string }) => {
+  const titleText = 'Простые правила разметки';
+  const isHomepage = slug === 'index';
+  const titleContent = isHomepage ? titleText : <a href='/'>{titleText}</a>;
+  const Element = isHomepage ? 'h1' : 'div';
 
-    <SiteNav slug={slug} />
-  </header>
-);
+  return (
+    <header className={styles.siteHeader}>
+      <Element className={styles.siteHeader__title}>{titleContent}</Element>
+
+      <SiteNav slug={slug} />
+    </header>
+  );
+};
 
 export default SiteHeader;
