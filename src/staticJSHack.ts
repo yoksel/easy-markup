7634dbecff6a24d02feb6ab1a3c2356f9eca492d
yoksel <juliahtml@gmail.com>
2023-12-23 00:48:11 +0000

    Check page states

--- src/staticJSHack.ts
+++ src/staticJSHack.ts
@@ -2,7 +2,18 @@ export default function () {
   if (typeof window === 'undefined') {
     return;
   }
+
   // https://blog.codepen.io/documentation/embedded-pens/#delayed-embeds
   (window as any).__CPEmbed?.();
+
   console.log('staticJSHack');
+  document.addEventListener('DOMContentLoaded', () => {
+    (window as any).__CPEmbed?.();
+    console.log('DOMContentLoaded');
+  });
+
+  document.addEventListener('readystatechange', () => {
+    (window as any).__CPEmbed?.();
+    console.log('readystatechange');
+  });
 }
